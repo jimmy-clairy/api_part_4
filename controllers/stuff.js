79f8b2323dc19dc1d50849d573348b9284e7d763
Jimmy Clairy <jimcl@hotmail.fr>
2022-08-18 13:03:53 +0200

    Modify controller modifyThing

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -16,33 +16,47 @@ exports.createThing = (req, res, next) => {
         .catch(error => { res.status(400).json({ error }) })
 };
 
+exports.modifyThing = (req, res) => {
+    // Vérifie si le fichier image a été modifié
+    if (req.file) {
+        Thing.findOne({ _id: req.params.id })
+            .then(thing => {
+                if (thing.userId != req.auth.userId) {
+                    res.status(401).json({ message: 'Not authorized' });
+                } else {
+                    // Supprime l'ancienne image
+                    const filename = thing.imageUrl.split('/images/')[1];
+                    fs.unlink(`images/${filename}`, () => {
+                        // Remplace par la nouvelle image
+                        const thingObject = {
+                            ...JSON.parse(req.body.thing),
+                            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+                        }
+                        // Mise a jour de l'objet avec la nouvelle image
+                        Thing.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
+                            .then(() => res.status(200).json({ message: 'Objet modifié!' }))
+                            .catch(error => res.status(400).json({ error }));
+                    })
+                }
 
-exports.getOneThing = ((req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-        .then(thing => res.status(200).json(thing))
-        .catch(error => res.status(404).json({ error }));
-});
-
-exports.modifyThing = (req, res, next) => {
-    const thingObject = req.file ? {
-        ...JSON.parse(req.body.thing),
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    } : { ...req.body };
-
-    delete thingObject._userId;
-    Thing.findOne({ _id: req.params.id })
-        .then((thing) => {
-            if (thing.userId != req.auth.userId) {
-                res.status(401).json({ message: 'Not authorized' });
-            } else {
-                Thing.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
-                    .then(() => res.status(200).json({ message: 'Objet modifié!' }))
-                    .catch(error => res.status(401).json({ error }));
-            }
-        })
-        .catch((error) => {
-            res.status(400).json({ error });
-        });
+            })
+            .catch(error => res.status(500).json({ error }));
+    } else {
+        const thingObject = { ...req.body };
+        Thing.findOne({ _id: req.params.id })
+            .then(thing => {
+                if (thing.userId != req.auth.userId) {
+                    res.status(401).json({ message: 'Not authorized' });
+                } else {
+                    Thing.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
+                        .then(() => res.status(200).json({ message: 'Objet modifié!' }))
+                        .catch(error => res.status(401).json({ error }));
+                }
+            })
+            .catch((error) => {
+                res.status(400).json({ error });
+            });
+    }
 };
 
 exports.deleteThing = (req, res, next) => {
@@ -68,6 +82,12 @@ exports.getAllThings = ((req, res, next) => {
     Thing.find()
         .then(things => res.status(200).json(things))
         .catch(error => res.status(400).json({ error }));
+});
+
+exports.getOneThing = ((req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+        .then(thing => res.status(200).json(thing))
+        .catch(error => res.status(404).json({ error }));
 });
 
 exports.deleteAllThings = ((req, res, next) => {
